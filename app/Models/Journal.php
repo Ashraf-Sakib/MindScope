534be917faffc50a3e4c33133f910544b11aebed
hasanAshraful <ashrafulhasan601@gmail.com>
2025-10-23 13:02:50 +0600

    tried to debug

--- app/Models/Journal.php
+++ app/Models/Journal.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Journal
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $content
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Journal whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Journal extends Model
 {
     use HasFactory;
--- app/Models/Mood.php
+++ app/Models/Mood.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Mood
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $mood
+ * @property string|null $note
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood whereMood($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood whereNote($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Mood whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Mood extends Model
 {
     use HasFactory;
